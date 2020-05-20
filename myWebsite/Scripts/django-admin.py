18e72d91106cd4b5e78d3fa89993e7ba2f902eee
Liling <35701243+yliling@users.noreply.github.com>
2020-05-19 23:09:11 -0400

    virtual env and start django

---
+++ myWebsite/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Liling Yuan\OneDrive - Virginia Tech\Virginia Tech\Resume\My Website\yliling.github.io\myWebsite\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
